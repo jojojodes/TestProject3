eaaf0b6808d6fe83fdff0482229a1f987306e3b9
Jody Eggleston <joeggleston@deloitte.com>
2022-02-03 06:35:15 -0500

    first commit for new project

--- functions.py
+++ functions.py
@@ -27,7 +27,7 @@ func_a('hello me!', 'hello you!')
 
 #________________________________________________________
 
-def func_b(text='default text'):
+def func_b(text='default text'): # all default text comes last.  (ie. func_b(text2, text='default text') vs func_b(text='default text', text2)
     print(text)
 
 func_b() # all text entered in this function being called will print to screen over specified in the def func_b() line.
@@ -41,3 +41,18 @@ func_b('custom text')  # for example func_b('custome text') will print 'custom t
     # hello you!
     # default text
     # custom text
+
+#___________________________________________________________
+
+def multiply(x, y):
+   # x * y    #on its own is a valid function but does not return any output.
+    return x * y
+
+print(multiply(2, 2))
+
+#______________________________________________________________
+
+def mulltipass(x):
+    print(x)
+
+multipass(1,2)
